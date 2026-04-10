a2dd30f7db28cd5a955977533d7a268485d5d646
Mathias Sonderfeld <mathias@sonderfeld.eu>
2026-04-10 12:08:52 +0200

    apply format

--- src/CgGeometry/cghalfedgeprimitives.cpp
+++ src/CgGeometry/cghalfedgeprimitives.cpp
@@ -1,21 +1,18 @@
 #include "cghalfedgeprimitives.h"
 
-CgHeFace::CgHeFace()
-{
-    m_edge=nullptr;
+CgHeFace::CgHeFace() {
+    m_edge = nullptr;
 }
 
-CgHeVert::CgHeVert()
-{
-    m_edge=nullptr;
+CgHeVert::CgHeVert() {
+    m_edge = nullptr;
 }
 
-CgHeEdge::CgHeEdge()
-{
-    m_next=nullptr;
-    m_pair=nullptr;
-    m_vert=nullptr;
-    m_face=nullptr;
+CgHeEdge::CgHeEdge() {
+    m_next = nullptr;
+    m_pair = nullptr;
+    m_vert = nullptr;
+    m_face = nullptr;
 }
 
 CgHeEdge::~CgHeEdge()
@@ -27,59 +24,48 @@ CgHeVert::~CgHeVert()
 CgHeFace::~CgHeFace()
 = default;
 
-const CgBaseHeEdge* CgHeFace::edge() const
-{
+const CgBaseHeEdge* CgHeFace::edge() const {
     return static_cast<CgBaseHeEdge*>(m_edge);
 }
 
-const glm::vec3 CgHeFace::center() const
-{
+const glm::vec3 CgHeFace::center() const {
     return (m_edge->vert()->position() + m_edge->next()->vert()->position() + m_edge->next()->next()->vert()->position()) / 3.0f;
 }
 
-const glm::vec3 CgHeFace::normal() const
-{
+const glm::vec3 CgHeFace::normal() const {
     return m_normal;
 }
 
-void CgHeFace::setNormal(const glm::vec3 normal)
-{
+void CgHeFace::setNormal(const glm::vec3 normal) {
     this->m_normal = normal;
 }
 
-const CgBaseHeVert* CgHeEdge::vert() const
-{
+const CgBaseHeVert* CgHeEdge::vert() const {
     return static_cast<CgBaseHeVert*>(m_vert);
 }
 
-const CgBaseHeEdge* CgHeEdge::next() const
-{
+const CgBaseHeEdge* CgHeEdge::next() const {
     return static_cast<CgBaseHeEdge*>(m_next);
 }
 
-const CgBaseHeEdge* CgHeEdge::pair() const
-{
+const CgBaseHeEdge* CgHeEdge::pair() const {
     return static_cast<CgBaseHeEdge*>(m_pair);
 }
 
-const CgBaseHeFace* CgHeEdge::face() const
-{
+const CgBaseHeFace* CgHeEdge::face() const {
     return static_cast<CgBaseHeFace*>(m_face);
 }
 
 
-const CgBaseHeEdge* CgHeVert::edge() const
-{
+const CgBaseHeEdge* CgHeVert::edge() const {
     return static_cast<CgBaseHeEdge*>(m_edge);
 }
 
-const glm::vec3 CgHeVert::position() const
-{
+const glm::vec3 CgHeVert::position() const {
     return m_position;
 }
 
-const glm::vec3 CgHeVert::color() const
-{
+const glm::vec3 CgHeVert::color() const {
     return m_color;
 }
 
@@ -87,68 +73,54 @@ const glm::vec3 CgHeVert::normal() const {
     return m_normal;
 }
 
-void CgHeVert::setNormal(const glm::vec3 normal)
-{
+void CgHeVert::setNormal(const glm::vec3 normal) {
     this->m_normal = normal;
 }
 
-int CgHeVert::index() const
-{
+int CgHeVert::index() const {
     return m_index;
 }
 
-void CgHeVert::setIndex(const int index)
-{
+void CgHeVert::setIndex(const int index) {
     this->m_index = index;
 }
 
-int CgHeEdge::startIndex() const
-{
+int CgHeEdge::startIndex() const {
     return m_start_index;
 }
 
-int CgHeEdge::endIndex() const
-{
+int CgHeEdge::endIndex() const {
     return m_end_index;
 }
 
-void CgHeEdge::setStartIndex(const int start_index)
-{
+void CgHeEdge::setStartIndex(const int start_index) {
     this->m_start_index = start_index;
 }
 
-void CgHeEdge::setEndIndex(const int end_index)
-{
+void CgHeEdge::setEndIndex(const int end_index) {
     this->m_end_index = end_index;
 }
 
-int CgHeFace::index1() const
-{
+int CgHeFace::index1() const {
     return m_index1;
 }
 
-int CgHeFace::index2() const
-{
+int CgHeFace::index2() const {
     return m_index2;
 }
 
-int CgHeFace::index3() const
-{
+int CgHeFace::index3() const {
     return m_index3;
 }
 
-void CgHeFace::setIndex1(const int index1)
-{
-    this->m_index1 =index1;
+void CgHeFace::setIndex1(const int index1) {
+    this->m_index1 = index1;
 }
 
-void CgHeFace::setIndex2(const int index2)
-{
+void CgHeFace::setIndex2(const int index2) {
     this->m_index2 = index2;
 }
 
-void CgHeFace::setIndex3(const int index3)
-{
+void CgHeFace::setIndex3(const int index3) {
     this->m_index3 = index3;
 }
-
